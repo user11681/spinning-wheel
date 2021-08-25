c89b7ca0ccbacc40efb3c97bc11855fff62a7892
auoeke <username11681@gmail.com>
2021-08-25 22:42:34 +0100

    use TestKit and clean up

---
+++ src/net/auoeke/wheel/util/GroovyUtil.java
@@ -0,0 +1,46 @@
+package net.auoeke.wheel.util;
+
+import groovy.lang.Closure;
+import java.util.Arrays;
+import java.util.List;
+import net.auoeke.reflect.Invoker;
+import org.codehaus.groovy.runtime.callsite.CallSite;
+import user11681.uncheck.Uncheck;
+
+public class GroovyUtil {
+    public static List<CallSite> sites(Class<?> type) {
+        return Arrays.asList(Invoker.invoke(Invoker.findStatic(type, "$getCallSiteArray", CallSite[].class)));
+    }
+
+    public static CallSite site(Class<?> type, int index) {
+        return sites(type).get(index);
+    }
+
+    public static Object property(List<CallSite> sites, Object object, int... indexes) {
+        for (int index : indexes) {
+            Object javaBad = object;
+            object = Uncheck.handle(() -> sites.get(index).callGetProperty(javaBad));
+        }
+
+        return object;
+    }
+
+    public static Object call(List<CallSite> sites, Object object, int... indexes) {
+        for (int index : indexes) {
+            Object javaBad = object;
+            object = Uncheck.handle(() -> sites.get(index).call(javaBad));
+        }
+
+        return object;
+    }
+
+    public static Closure<Void> closure(Runnable runnable) {
+        return new Closure<Void>(null) {
+            public Void doCall() {
+                runnable.run();
+
+                return null;
+            }
+        };
+    }
+}
